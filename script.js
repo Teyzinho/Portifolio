5b381deb3786aa599ee48a385d1e259daadbec8e
Thiago <teyextreme@outlook.com>
2023-04-18 12:57:29 -0300

    atualizacao cards

--- script.js
+++ script.js
@@ -112,3 +112,52 @@ function carrocel() {
         }, 400); 
     }
 }
+
+//Scroll animation
+
+//Otimização do javaScript
+const debounce = function(func, wait, immediate) {
+    let timeout;
+    return function(...args) {
+      const context = this;
+      const later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
+
+
+
+
+const alvo = document.querySelectorAll ('[data-anime]');
+const classeAnimacao = 'animar';
+
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight *2.5) /4); //constante que verifica a distância da barra de Scroll do topo
+    alvo.forEach(function(element){ //Mostra a distância da barra de scroll do topo de cada elemento
+        if((windowTop) > element.offsetTop){
+            element.classList.add(classeAnimacao);
+        } else {
+            element.classList.remove(classeAnimacao);
+        }
+
+        //console.log(element.offsetTop); Mostra a distância da barra de scroll do topo dos elementos
+    })
+    
+   // console.log(windowTop) //Mostra a distância da barra de scroll do topo
+}
+
+animeScroll();
+
+if(alvo.length) { //se Não tiver objeto animado a função fica desabilitada
+window.addEventListener('scroll', debounce(function(){
+    animeScroll();
+}, 100));
+}
